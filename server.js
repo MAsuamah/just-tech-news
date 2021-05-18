2735a2a44a58dfb61dd1dbf94a8c95b6cb50ba66
Michelle <michey.asmah21@gmail.com>
2021-05-17 23:22:18 -0400

    login and logout

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 
 app.engine('handlebars', hbs.engine);
@@ -18,6 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
